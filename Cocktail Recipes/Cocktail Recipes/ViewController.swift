56331a2fd4c9535057f4210b41cd9607ecf89da4
Zhenya Loyko <e.lojko@inbox.ru>
2023-11-11 14:19:27 +0300

    Project created

---
+++ Cocktail Recipes/Cocktail Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktail Recipes
+//
+//  Created by Евгений Лойко on 11.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
