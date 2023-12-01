bd41004a5fd446c3f0ee1d8626b3b27a2e172d19
Zhenya Loyko <e.lojko@inbox.ru>
2023-12-02 00:17:59 +0300

    Better way to show Ingredients

--- Cocktail Recipes/Cocktail Recipes/Controllers/DrinkDetailVC.swift
+++ Cocktail Recipes/Cocktail Recipes/Controllers/DrinkDetailVC.swift
@@ -12,8 +12,7 @@ class DrinkDetailVC: UIViewController {
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var strDrink: UILabel!
-    @IBOutlet private var ingredientsLbls: [UILabel]!
-    @IBOutlet private var measuresLbls: [UILabel]!
+    @IBOutlet var ingredientsLbls: [UILabel]!
     @IBOutlet private weak var strInstructions: UILabel!
     
     var drink: Drink? { didSet { getThumbnailUrl() } }
@@ -64,47 +63,24 @@ class DrinkDetailVC: UIViewController {
         ingredientsLbls.forEach { ingredientLbl in
             guard let text = ingredientLbl.text else { return }
             switch text {
-                case "strIngredient1": ingredientLbl.text = updatedDrink?.strIngredient1
-                case "strIngredient2": ingredientLbl.text = updatedDrink?.strIngredient2
-                case "strIngredient3": ingredientLbl.text = updatedDrink?.strIngredient3
-                case "strIngredient4": ingredientLbl.text = updatedDrink?.strIngredient4
-                case "strIngredient5": ingredientLbl.text = updatedDrink?.strIngredient5
-                case "strIngredient6": ingredientLbl.text = updatedDrink?.strIngredient6
-                case "strIngredient7": ingredientLbl.text = updatedDrink?.strIngredient7
-                case "strIngredient8": ingredientLbl.text = updatedDrink?.strIngredient8
-                case "strIngredient9": ingredientLbl.text = updatedDrink?.strIngredient9
-                case "strIngredient10": ingredientLbl.text = updatedDrink?.strIngredient10
-                case "strIngredient11": ingredientLbl.text = updatedDrink?.strIngredient11
-                case "strIngredient12": ingredientLbl.text = updatedDrink?.strIngredient12
-                case "strIngredient13": ingredientLbl.text = updatedDrink?.strIngredient13
-                case "strIngredient14": ingredientLbl.text = updatedDrink?.strIngredient14
-                case "strIngredient15": ingredientLbl.text = updatedDrink?.strIngredient15
+                case "1": ingredientLbl.text = (updatedDrink?.strMeasure1 ?? "") + (updatedDrink?.strIngredient1 ?? "")
+                case "2": ingredientLbl.text = (updatedDrink?.strMeasure2 ?? "") + (updatedDrink?.strIngredient2 ?? "")
+                case "3": ingredientLbl.text = (updatedDrink?.strMeasure3 ?? "") + (updatedDrink?.strIngredient3 ?? "")
+                case "4": ingredientLbl.text = (updatedDrink?.strMeasure4 ?? "") + (updatedDrink?.strIngredient4 ?? "")
+                case "5": ingredientLbl.text = (updatedDrink?.strMeasure5 ?? "") + (updatedDrink?.strIngredient5 ?? "")
+                case "6": ingredientLbl.text = (updatedDrink?.strMeasure6 ?? "") + (updatedDrink?.strIngredient6 ?? "")
+                case "7": ingredientLbl.text = (updatedDrink?.strMeasure7 ?? "") + (updatedDrink?.strIngredient7 ?? "")
+                case "8": ingredientLbl.text = (updatedDrink?.strMeasure8 ?? "") + (updatedDrink?.strIngredient8 ?? "")
+                case "9": ingredientLbl.text = (updatedDrink?.strMeasure9 ?? "") + (updatedDrink?.strIngredient9 ?? "")
+                case "10": ingredientLbl.text = (updatedDrink?.strMeasure10 ?? "") + (updatedDrink?.strIngredient10 ?? "")
+                case "11": ingredientLbl.text = (updatedDrink?.strMeasure11 ?? "") + (updatedDrink?.strIngredient11 ?? "")
+                case "12": ingredientLbl.text = (updatedDrink?.strMeasure12 ?? "") + (updatedDrink?.strIngredient12 ?? "")
+                case "13": ingredientLbl.text = (updatedDrink?.strMeasure13 ?? "") + (updatedDrink?.strIngredient13 ?? "")
+                case "14": ingredientLbl.text = (updatedDrink?.strMeasure14 ?? "") + (updatedDrink?.strIngredient14 ?? "")
+                case "15": ingredientLbl.text = (updatedDrink?.strMeasure15 ?? "") + (updatedDrink?.strIngredient15 ?? "")
             default: break
             }
-            ingredientLbl.isHidden = ingredientLbl.text != nil ? false : true
-        }
-        
-        measuresLbls.forEach { measureLbl in
-            guard let text = measureLbl.text else { return }
-            switch text {
-                case "strMeasure1": measureLbl.text = updatedDrink?.strMeasure1
-                case "strMeasure2": measureLbl.text = updatedDrink?.strMeasure2
-                case "strMeasure3": measureLbl.text = updatedDrink?.strMeasure3
-                case "strMeasure4": measureLbl.text = updatedDrink?.strMeasure4
-                case "strMeasure5": measureLbl.text = updatedDrink?.strMeasure5
-                case "strMeasure6": measureLbl.text = updatedDrink?.strMeasure6
-                case "strMeasure7": measureLbl.text = updatedDrink?.strMeasure7
-                case "strMeasure8": measureLbl.text = updatedDrink?.strMeasure8
-                case "strMeasure9": measureLbl.text = updatedDrink?.strMeasure9
-                case "strMeasure10": measureLbl.text = updatedDrink?.strMeasure10
-                case "strMeasure11": measureLbl.text = updatedDrink?.strMeasure11
-                case "strMeasure12": measureLbl.text = updatedDrink?.strMeasure12
-                case "strMeasure13": measureLbl.text = updatedDrink?.strMeasure13
-                case "strMeasure14": measureLbl.text = updatedDrink?.strMeasure14
-                case "strMeasure15": measureLbl.text = updatedDrink?.strMeasure15
-            default: break
-            }
-            measureLbl.isHidden = measureLbl.text != nil ? false : true
+            ingredientLbl.isHidden = ingredientLbl.text != "" ? false : true
         }
         
         strInstructions.text = updatedDrink?.strInstructions
