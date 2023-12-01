b41f3327f55c65ca8419cd07fa653e01c395f07e
Zhenya Loyko <e.lojko@inbox.ru>
2023-12-01 21:58:07 +0300

    Optimization of ingredients

--- Cocktail Recipes/Cocktail Recipes/Controllers/DrinkDetailVC.swift
+++ Cocktail Recipes/Cocktail Recipes/Controllers/DrinkDetailVC.swift
@@ -12,23 +12,7 @@ class DrinkDetailVC: UIViewController {
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var strDrink: UILabel!
-    
-    @IBOutlet private weak var strIngredient1: UILabel!
-    @IBOutlet private weak var strIngredient2: UILabel!
-    @IBOutlet private weak var strIngredient3: UILabel!
-    @IBOutlet private weak var strIngredient4: UILabel!
-    @IBOutlet private weak var strIngredient5: UILabel!
-    @IBOutlet private weak var strIngredient6: UILabel!
-    @IBOutlet private weak var strIngredient7: UILabel!
-    @IBOutlet private weak var strIngredient8: UILabel!
-    @IBOutlet private weak var strIngredient9: UILabel!
-    @IBOutlet private weak var strIngredient10: UILabel!
-    @IBOutlet private weak var strIngredient11: UILabel!
-    @IBOutlet private weak var strIngredient12: UILabel!
-    @IBOutlet private weak var strIngredient13: UILabel!
-    @IBOutlet private weak var strIngredient14: UILabel!
-    @IBOutlet private weak var strIngredient15: UILabel!
-    
+    @IBOutlet private var ingredientsLbls: [UILabel]!
     @IBOutlet private weak var strInstructions: UILabel!
     
     var drink: Drink? { didSet { getThumbnailUrl() } }
@@ -75,36 +59,28 @@ class DrinkDetailVC: UIViewController {
     }
     
     private func setupDrinkDetail() {
-        strIngredient1.text = updatedDrink?.strIngredient1
-        strIngredient1.isHidden = strIngredient1.text != nil ? false : true
-        strIngredient2.text = updatedDrink?.strIngredient2
-        strIngredient2.isHidden = strIngredient2.text != nil ? false : true
-        strIngredient3.text = updatedDrink?.strIngredient3
-        strIngredient3.isHidden = strIngredient3.text != nil ? false : true
-        strIngredient4.text = updatedDrink?.strIngredient4
-        strIngredient4.isHidden = strIngredient4.text != nil ? false : true
-        strIngredient5.text = updatedDrink?.strIngredient5
-        strIngredient5.isHidden = strIngredient5.text != nil ? false : true
-        strIngredient6.text = updatedDrink?.strIngredient6
-        strIngredient6.isHidden = strIngredient6.text != nil ? false : true
-        strIngredient7.text = updatedDrink?.strIngredient7
-        strIngredient7.isHidden = strIngredient7.text != nil ? false : true
-        strIngredient8.text = updatedDrink?.strIngredient8
-        strIngredient8.isHidden = strIngredient8.text != nil ? false : true
-        strIngredient9.text = updatedDrink?.strIngredient9
-        strIngredient9.isHidden = strIngredient9.text != nil ? false : true
-        strIngredient10.text = updatedDrink?.strIngredient10
-        strIngredient10.isHidden = strIngredient10.text != nil ? false : true
-        strIngredient11.text = updatedDrink?.strIngredient11
-        strIngredient11.isHidden = strIngredient11.text != nil ? false : true
-        strIngredient12.text = updatedDrink?.strIngredient12
-        strIngredient12.isHidden = strIngredient12.text != nil ? false : true
-        strIngredient13.text = updatedDrink?.strIngredient13
-        strIngredient13.isHidden = strIngredient13.text != nil ? false : true
-        strIngredient14.text = updatedDrink?.strIngredient14
-        strIngredient14.isHidden = strIngredient14.text != nil ? false : true
-        strIngredient15.text = updatedDrink?.strIngredient15
-        strIngredient15.isHidden = strIngredient15.text != nil ? false : true
+        ingredientsLbls.forEach { ingredientLbl in
+            guard let text = ingredientLbl.text else { return }
+            switch text {
+                case "strIngredient1": ingredientLbl.text = updatedDrink?.strIngredient1
+                case "strIngredient2": ingredientLbl.text = updatedDrink?.strIngredient2
+                case "strIngredient3": ingredientLbl.text = updatedDrink?.strIngredient3
+                case "strIngredient4": ingredientLbl.text = updatedDrink?.strIngredient4
+                case "strIngredient5": ingredientLbl.text = updatedDrink?.strIngredient5
+                case "strIngredient6": ingredientLbl.text = updatedDrink?.strIngredient6
+                case "strIngredient7": ingredientLbl.text = updatedDrink?.strIngredient7
+                case "strIngredient8": ingredientLbl.text = updatedDrink?.strIngredient8
+                case "strIngredient9": ingredientLbl.text = updatedDrink?.strIngredient9
+                case "strIngredient10": ingredientLbl.text = updatedDrink?.strIngredient10
+                case "strIngredient11": ingredientLbl.text = updatedDrink?.strIngredient11
+                case "strIngredient12": ingredientLbl.text = updatedDrink?.strIngredient12
+                case "strIngredient13": ingredientLbl.text = updatedDrink?.strIngredient13
+                case "strIngredient14": ingredientLbl.text = updatedDrink?.strIngredient14
+                case "strIngredient15": ingredientLbl.text = updatedDrink?.strIngredient15
+            default: break
+            }
+            ingredientLbl.isHidden = ingredientLbl.text != nil ? false : true
+        }
         
         strInstructions.text = updatedDrink?.strInstructions
     }
