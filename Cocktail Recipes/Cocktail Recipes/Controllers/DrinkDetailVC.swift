b9f113cd5a1c587c066759d00d6d26a95b5b4ad7
Zhenya Loyko <e.lojko@inbox.ru>
2023-12-01 19:09:33 +0300

    Showing ingredients

--- Cocktail Recipes/Cocktail Recipes/Controllers/DrinkDetailVC.swift
+++ Cocktail Recipes/Cocktail Recipes/Controllers/DrinkDetailVC.swift
@@ -12,6 +12,23 @@ class DrinkDetailVC: UIViewController {
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var strDrink: UILabel!
+    
+    @IBOutlet private weak var strIngredient1: UILabel!
+    @IBOutlet private weak var strIngredient2: UILabel!
+    @IBOutlet private weak var strIngredient3: UILabel!
+    @IBOutlet private weak var strIngredient4: UILabel!
+    @IBOutlet private weak var strIngredient5: UILabel!
+    @IBOutlet private weak var strIngredient6: UILabel!
+    @IBOutlet private weak var strIngredient7: UILabel!
+    @IBOutlet private weak var strIngredient8: UILabel!
+    @IBOutlet private weak var strIngredient9: UILabel!
+    @IBOutlet private weak var strIngredient10: UILabel!
+    @IBOutlet private weak var strIngredient11: UILabel!
+    @IBOutlet private weak var strIngredient12: UILabel!
+    @IBOutlet private weak var strIngredient13: UILabel!
+    @IBOutlet private weak var strIngredient14: UILabel!
+    @IBOutlet private weak var strIngredient15: UILabel!
+    
     @IBOutlet private weak var strInstructions: UILabel!
     
     var drink: Drink? { didSet { getThumbnailUrl() } }
@@ -53,9 +70,42 @@ class DrinkDetailVC: UIViewController {
         }.resume()
     }
     
-    private func setupUI() { strDrink.text = drink?.strDrink }
+    private func setupUI() {
+        strDrink.text = drink?.strDrink
+    }
     
     private func setupDrinkDetail() {
+        strIngredient1.text = updatedDrink?.strIngredient1
+        strIngredient1.isHidden = strIngredient1.text != nil ? false : true
+        strIngredient2.text = updatedDrink?.strIngredient2
+        strIngredient2.isHidden = strIngredient2.text != nil ? false : true
+        strIngredient3.text = updatedDrink?.strIngredient3
+        strIngredient3.isHidden = strIngredient3.text != nil ? false : true
+        strIngredient4.text = updatedDrink?.strIngredient4
+        strIngredient4.isHidden = strIngredient4.text != nil ? false : true
+        strIngredient5.text = updatedDrink?.strIngredient5
+        strIngredient5.isHidden = strIngredient5.text != nil ? false : true
+        strIngredient6.text = updatedDrink?.strIngredient6
+        strIngredient6.isHidden = strIngredient6.text != nil ? false : true
+        strIngredient7.text = updatedDrink?.strIngredient7
+        strIngredient7.isHidden = strIngredient7.text != nil ? false : true
+        strIngredient8.text = updatedDrink?.strIngredient8
+        strIngredient8.isHidden = strIngredient8.text != nil ? false : true
+        strIngredient9.text = updatedDrink?.strIngredient9
+        strIngredient9.isHidden = strIngredient9.text != nil ? false : true
+        strIngredient10.text = updatedDrink?.strIngredient10
+        strIngredient10.isHidden = strIngredient10.text != nil ? false : true
+        strIngredient11.text = updatedDrink?.strIngredient11
+        strIngredient11.isHidden = strIngredient11.text != nil ? false : true
+        strIngredient12.text = updatedDrink?.strIngredient12
+        strIngredient12.isHidden = strIngredient12.text != nil ? false : true
+        strIngredient13.text = updatedDrink?.strIngredient13
+        strIngredient13.isHidden = strIngredient13.text != nil ? false : true
+        strIngredient14.text = updatedDrink?.strIngredient14
+        strIngredient14.isHidden = strIngredient14.text != nil ? false : true
+        strIngredient15.text = updatedDrink?.strIngredient15
+        strIngredient15.isHidden = strIngredient15.text != nil ? false : true
+        
         strInstructions.text = updatedDrink?.strInstructions
     }
     
