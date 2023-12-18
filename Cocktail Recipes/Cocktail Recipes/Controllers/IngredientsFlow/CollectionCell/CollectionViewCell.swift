606f79328f05f3efb13102b89e17cd799bca2ab1
Zhenya Loyko <e.lojko@inbox.ru>
2023-12-18 20:53:17 +0300

    Little changes

--- Cocktail Recipes/Cocktail Recipes/Controllers/IngredientsFlow/CollectionCell/CollectionViewCell.swift
+++ Cocktail Recipes/Cocktail Recipes/Controllers/IngredientsFlow/CollectionCell/CollectionViewCell.swift
@@ -15,6 +15,7 @@ class CollectionViewCell: UICollectionViewCell {
     
     var thumbnailUrl: String? {
         didSet {
+            imageView.image = nil
             getThumbnailUrl()
         }
     }
